a62ea920b1c39f4a14a443b7176ebb6bac5cb470
Yusuf Martinus Arief <brothanes11@gmail.com>
2024-06-07 23:32:01 +0700

    Add logout logic in profile fragment and changing font

--- app/src/main/java/com/example/designify/ui/user/botnavbar/ProfileFragment.kt
+++ app/src/main/java/com/example/designify/ui/user/botnavbar/ProfileFragment.kt
@@ -1,5 +1,6 @@
 package com.example.designify.ui.user.botnavbar
 
+import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import androidx.fragment.app.Fragment
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.designify.databinding.FragmentProfileBinding
+import com.example.designify.ui.welcome.WelcomeActivity
 
 class ProfileFragment : Fragment() {
 
@@ -20,17 +22,23 @@ class ProfileFragment : Fragment() {
         return binding.root
     }
 
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-//
-//        val username = arguments?.getString("username")
-//        val totalDownload = arguments?.getString("totalDownload")
-//
-//        Log.d("ProfileFragment", "Username: $username")
-//
-//        with(binding) {
-//            tvUsername.text = username
-//            tvTotalDownloads.text = totalDownload
-//        }
-//    }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val username = arguments?.getString("username")
+        val totalDownload = arguments?.getString("totalDownload")
+
+        Log.d("ProfileFragment", "Username: $username")
+
+        with(binding) {
+            tvUsername.text = username ?: "Default Username"
+            tvNum.text = totalDownload ?: 0.toString()
+
+            btnLogout.setOnClickListener {
+                val intent = Intent(requireContext(), WelcomeActivity::class.java)
+                startActivity(intent)
+                requireActivity().finish()
+            }
+        }
+    }
 }
